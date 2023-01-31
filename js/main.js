fce703cf6c80214e5d58370897762f840e3abae2
unknown <ragimov.rehim@gmail.com>
2023-01-31 14:35:26 +0400

    JS

---
+++ js/main.js
@@ -0,0 +1,47 @@
+// task1
+var a = 10
+var b = 2
+var result = 0
+result = a + b
+
+console.log(result)
+
+// task2
+var a = 5
+prompt("eded daxil edin", b)
+var result = 0
+result = a * b
+
+console.log(result)
+
+
+// task3
+var a = 10 
+prompt("eded daxil edin", b)
+var result = 0 
+result = a * b
+
+if(result > 0){
+    console.log("musbet ededdir")
+}
+if(result = 0){
+    console.log("cavab. 0.dir")
+}
+{console.log("menfi ededdir")
+}
+
+// task4
+
+
+
+
+
+
+
+
+
+
+
+
+
+
