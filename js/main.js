760055231c7f18feb18019957ace7fea04ba0c6a
unknown <ragimov.rehim@gmail.com>
2023-02-02 18:25:00 +0400

    01.02.23 tasks

--- js/main.js
+++ js/main.js
@@ -1,36 +1,194 @@
 // task1
-var a = 10
-var b = 2
-var result = 0
-result = a + b
+// var a = 10
+// var b = 2
+// var result = 0
+// result = a + b
 
-console.log(result)
+// console.log(result)
 
 // task2
-var a = 5
-prompt("eded daxil edin", b)
-var result = 0
-result = a * b
+// var a = 5
+// prompt("eded daxil edin", b)
+// var result = 0
+// result = a * b
 
-console.log(result)
+// console.log(result)
 
 
 // task3
-var a = 10 
-prompt("eded daxil edin", b)
-var result = 0 
-result = a * b
+// var a = 10 
+// var b =prompt("eded daxil edin")
+// var result = 0 
+// result = a * b
 
-if(result > 0){
-    console.log("musbet ededdir")
-}
-if(result = 0){
-    console.log("cavab. 0.dir")
-}
-{console.log("menfi ededdir")
-}
+// if(result > 0){
+//     console.log("musbet ededdir")
+// }
+// else if(result = 0){
+//     console.log("cavab. 0.dir")
+// } else {
+//     console.log("menfi ededdir")
+// }
+
+// >>>>>>>>>>>>>>NEW tasks 01.02.23<<<<<<<<<<<<<<<<<
+
+// task1
+// 10 ile 200 arasında ededlerin 3 qatinin 2 eded artiginin 5-e bölünen ededleri gösterin
+// for(let i = 10; i < 200; i ++){
+//     if((i *3+2)% 5 == 0)
+//     {alert(i)}
+// }
+
+
+// task2
+// 1-100 arasındaki cut ededlerin toplamını tapin
+// a = 0
+// for(let i = 1; i < 100; i ++)
+// {
+//     if( i % 2 == 0)
+//     a += i
+// }
+// console.log(a)
+
+
+// task3
+// input ile 5 eded daxil edin. 7'ye bolunenlerın sayini tapib ekrana yazdırin
+// var i = 0
+// for( counter = 0; counter <= 5; counter++)
+// { var a = +prompt("eded daxil edin")
+// var result = a % 7 
+// if( result == 0)
+// i += 1
+// }
+// console.log(i)
+ 
 
 // task4
+// Input ile daxil olunan ədədin bölənlərini tapın.
+// let a = +prompt("eded daxil edin")
+// for(bolen = 1; bolen <= a; bolen ++)
+// {
+//     result = a % bolen
+//     if(result == 0){
+//         console.log(bolen)
+//     }
+// }
+
+
+// task5
+// Input ile daxil olunan ədədin bölənlərinin cemini tapın.
+// let a = +prompt("eded daxil edin")
+// let cem = 0
+// for(bolen = 1; bolen <= a; bolen ++)
+// {
+//     result = a % bolen
+//     if(result == 0){
+//         cem += bolen
+//     }
+// }
+// console.log(cem)
+
+
+
+// task6
+// Input ile daxil olunan ədədin bölənlərinin hasilini tapın
+// let a = +prompt("eded daxil edin")
+// let hasil = 1
+// for(bolen = 1; bolen <= a; bolen ++)
+// {
+//     result = a % bolen
+//     if(result == 0){
+//         hasil = hasil * bolen
+//     }
+// }
+// console.log("Input ile daxil olunan ədədin bölənlərinin hasili", hasil)
+
+
+// task7
+//  Input ile daxil olunan ədədin neçə rəqəmli olduğunu tapın.
+// let eded = +prompt("eded daxil edin");
+// while(eded > 0){
+//     let digit = eded % 10
+//     eded = Math.trunc(eded/10)
+//     console.log(digit)
+// }
+
+
+// task8
+// Input ile daxil olunan ədədin rəqəmlərinin cəmini tapın
+// let eded = +prompt("eded daxil edin");
+// let cem = 0
+// while(eded > 0){
+//     let digit = eded % 10
+//     eded = Math.trunc(eded/10)
+//     cem += digit
+// }
+// console.log("Input ile daxil olunan ədədin rəqəmlərinin cəmi", cem)
+
+
+// task9
+// Input ile daxil olunan ədədin rəqəmlərinin hasilini tapın
+// var eded = +prompt("eded daxil edin")
+// var hasil = 1
+// while(eded > 0){
+//   var reminder = eded % 10
+//     eded = Math.trunc(eded/10)
+//     hasil = hasil * reminder 
+// }
+// console.log("input ile daxil olunan ədədin rəqəmlərinin hasilini tapın", hasil)
+
+
+// task10
+// Input ile daxil olunan ədədin sadə yoxsa mürəkkəb olduğunu tapın.
+
+
+// task11
+// input ile daxil olan 3 eded arasinda en boyuyunu tapin.
+// var a = +prompt("1-ci eded daxil edin")
+// var b = +prompt("2-ci ededi daxil edin")
+// var c = +prompt("3-cu ededi daxil edin")
+
+// if(a > b  && a > c)
+// { 
+// console.log("En boyuk eded:", a)
+// }
+// else if( b > a && b > c)
+// {
+//     console.log("en boyuk eded:", b)
+// }
+// else if( c > a && c > b )
+// { 
+//     console.log("en boiyuk eded:", c)
+// }
+
+
+// task12
+// input ile daxil olunan 3 ededden ortancili tapan funksiya yazin.
+// let a = +prompt("eded daxil edin")
+// let b = +prompt("eded daxil edin")
+// let c = +prompt("eded daxil edin")
+
+// if(a > b && b > c)
+// {console.log("ortancil eded:", b)}
+// else if(b > a && a > c)
+// {
+//     console.log("ortancil eded:", a)
+// }
+// else if( a > c && c > b)
+// {
+//     console.log("ortancil eded:", c)
+// }
+
+
+
+
+
+
+
+
+
+
+
 
 
 
